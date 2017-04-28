d20cfc4824449f112db912efe1eab24ff8416f47
jaudoux <jerome.audoux@gmail.com>
2017-04-28 08:47:43 +0200

    Add unstranded mode

--- mergeTags.c
+++ mergeTags.c
@@ -24,11 +24,17 @@ typedef struct {
   char     *seq;
 } assembly_t;
 
+typedef struct {
+  uint32_t assembly_id;
+  uint8_t  revcomp;
+} assembly_kmer_t;
+
 void assembly_destroy(assembly_t *a) {
   if(a->seq)
     free(a->seq);
   free(a);
 }
+
 
 int cmp_assembly(const void * a, const void * b) {
   const assembly_t *ass_a = *(const assembly_t **)a;
@@ -41,20 +47,36 @@ int cmp_assembly(const void * a, const void * b) {
 }
 
 // Init k-mers hash
-KHASH_MAP_INIT_INT64(kmers, uint32_t)
+KHASH_MAP_INIT_INT64(kmers, assembly_kmer_t)
 
 typedef khash_t(kmers) kmers_hash_t;
 typedef kvec_t(assembly_t*) assemblies_array_t;
+
+void add_assembly_kmer(kmers_hash_t *h, uint64_t kmer, uint32_t assembly_id, uint8_t rc) {
+  khiter_t k = kh_get(kmers, h, kmer);
+  int dret = 0;
+  if(k == kh_end(h)) {
+    k = kh_put(kmers, h, kmer, &dret);
+    kh_value(h, k).assembly_id = assembly_id + 1;
+    kh_value(h, k).revcomp = rc;
+  } else {
+    kh_value(h, k).assembly_id = 0;
+  }
+}
 
 int main(int argc, char *argv[])
 {
   char *counts_file;
   int k_length = 31;
+  int stranded = 1;
+  int min_assembly_k = MIN_ASSEMBLY_K;
 
   int c;
-  while ((c = getopt(argc, argv, "k:")) >= 0) {
+  while ((c = getopt(argc, argv, "nk:m:")) >= 0) {
     switch (c) {
+      case 'n': stranded = 0; break;
       case 'k': k_length = atoi(optarg); break;
+      case 'm': min_assembly_k = atoi(optarg); break;
     }
   }
 
@@ -62,6 +84,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "\n");
 		fprintf(stderr, "Usage:   mergeTags [options] <counts.tsv>\n\n");
 		fprintf(stderr, "Options: -k INT    length of k-mers (max_value: 32) [%d]\n", k_length);
+		fprintf(stderr, "         -m INT    min assembly overlap (max_value: k) [%d]\n", min_assembly_k);
+		fprintf(stderr, "         -n        Unstranded merging procedure\n");
 		fprintf(stderr, "\n");
 		return 1;
 	}
@@ -107,7 +131,7 @@ int main(int argc, char *argv[])
   fprintf(stderr, "%d k-mers loaded\n", nb_kmers);
   fprintf(stderr, "Assembling k-mers\n");
 
-  for(int i = k_length - 1; i >= MIN_ASSEMBLY_K; i--) {
+  for(int i = k_length - 1; i >= min_assembly_k; i--) {
     int has_merge_assemblies = 1;
     while(has_merge_assemblies) {
       has_merge_assemblies = 0;
@@ -127,21 +151,32 @@ int main(int argc, char *argv[])
         if(strlen(assembly->seq) <= i) continue;
         uint64_t left_kmer  = dna_to_int(assembly->seq, i);
         uint64_t right_kmer = dna_to_int(&assembly->seq[strlen(assembly->seq) - i], i);
+        uint8_t is_left_kmer_rc = 0, is_right_kmer_rc = 0;
 
-        k = kh_get(kmers, left_h, left_kmer);
-        if(k == kh_end(left_h)) {
-          k = kh_put(kmers, left_h, left_kmer, &dret);
-          kh_value(left_h, k) = j + 1;
-        } else {
-          kh_value(left_h, k) = 0;
+        // If we are not in stranded mode, we index the canonical k-i k-mers
+        if(!stranded) {
+          uint64_t left_kmer_rc = int_revcomp(left_kmer, i);
+          uint64_t right_kmer_rc = int_revcomp(right_kmer, i);
+          if(left_kmer_rc < left_kmer) {
+            left_kmer = left_kmer_rc;
+            is_left_kmer_rc = 1;
+          }
+          if(right_kmer_rc < right_kmer) {
+            right_kmer = right_kmer_rc;
+            is_right_kmer_rc = 1;
+          }
         }
 
-        k = kh_get(kmers, right_h, right_kmer);
-        if(k == kh_end(right_h)) {
-          k = kh_put(kmers, right_h, right_kmer, &dret);
-          kh_value(right_h, k) = j + 1;
+        if(is_left_kmer_rc) {
+          add_assembly_kmer(right_h, left_kmer, j, is_left_kmer_rc);
         } else {
-          kh_value(right_h, k) = 0;
+          add_assembly_kmer(left_h, left_kmer, j, is_left_kmer_rc);
+        }
+
+        if(is_right_kmer_rc) {
+          add_assembly_kmer(left_h, right_kmer, j, is_right_kmer_rc);
+        } else {
+          add_assembly_kmer(right_h, right_kmer, j, is_right_kmer_rc);
         }
       }
 
@@ -149,19 +184,21 @@ int main(int argc, char *argv[])
       // Now we try to merge assemblies
       for(k = kh_begin(right_h); k != kh_end(right_h); ++k) {
         if(!kh_exist(right_h, k)) continue;
-        if(kh_value(right_h, k) == 0) continue;
 
-        assembly_t *left_assembly = kv_A(*a, kh_value(right_h, k) - 1);
+        if(kh_value(right_h, k).assembly_id == 0) continue;
+
+        assembly_t *left_assembly = kv_A(*a, kh_value(right_h, k).assembly_id - 1);
 
         if(!left_assembly) continue;
 
         k2 = kh_get(kmers, left_h, kh_key(right_h, k));
 
-        if(k2 != kh_end(left_h) && kh_value(left_h, k2) != 0 && kh_value(left_h, k2) != kh_value(right_h, k)) {
+        if(k2 != kh_end(left_h) && kh_value(left_h, k2).assembly_id != 0 && kh_value(left_h, k2).assembly_id != kh_value(right_h, k).assembly_id) {
 
           has_merge_assemblies = 1;
+
           // Merge the two sequences into a new assembly
-          assembly_t *right_assembly = kv_A(*a, kh_value(left_h, k2) - 1);
+          assembly_t *right_assembly = kv_A(*a, kh_value(left_h, k2).assembly_id - 1);
 
           if(!right_assembly) continue;
 
@@ -174,7 +211,30 @@ int main(int argc, char *argv[])
             assemblies_merge = right_assembly;
             removed_assembly = left_assembly;
           }
+
+          // Create a new string for the assembly merging
           char *merge_seq = malloc(strlen(right_assembly->seq) + strlen(left_assembly->seq) - i + 1);
+
+          // If the assemblies are not in the same orientation, we reverse one of them
+          if(kh_value(left_h, k2).revcomp != kh_value(right_h, k).revcomp) {
+            if(kh_value(left_h, k2).revcomp) {
+              char* buffer = malloc(strlen(right_assembly->seq) + 1);
+              revcomp(right_assembly->seq, buffer, strlen(right_assembly->seq));
+              free(right_assembly->seq);
+              right_assembly->seq = buffer;
+            } else {
+              char* buffer = malloc(strlen(left_assembly->seq) + 1);
+              revcomp(left_assembly->seq, buffer, strlen(left_assembly->seq));
+              free(left_assembly->seq);
+              left_assembly->seq = buffer;
+            }
+          } else if(kh_value(left_h, k2).revcomp && kh_value(right_h, k).revcomp) {
+            // If both assemblies are in RC, we swap them
+            assembly_t *tmp_assembly = left_assembly;
+            left_assembly = right_assembly;
+            right_assembly = tmp_assembly;
+          }
+
           strcpy(merge_seq, left_assembly->seq);
           strcat(merge_seq, &right_assembly->seq[i]);
 
@@ -187,8 +247,8 @@ int main(int argc, char *argv[])
           assembly_destroy(removed_assembly);
 
           kv_push(assembly_t*, *new_a, assemblies_merge);
-          kv_A(*a, kh_value(right_h, k) - 1) = NULL;
-          kv_A(*a, kh_value(left_h, k2) - 1) = NULL;
+          kv_A(*a, kh_value(right_h, k).assembly_id - 1) = NULL;
+          kv_A(*a, kh_value(left_h, k2).assembly_id - 1) = NULL;
         }
       }
 
